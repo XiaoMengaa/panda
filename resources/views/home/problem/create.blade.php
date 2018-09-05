a854124ed20c438e217883498c164e43be8805d0
hanguoxi <1054127804@qq.com>
2018-09-05 14:55:28 +0800

    前台

--- resources/views/home/problem/create.blade.php
+++ resources/views/home/problem/create.blade.php
@@ -5,18 +5,29 @@
 <style>
 
 </style>
-<form class="form-horizontal" action="/problem" method="post">
+<form class="form-horizontal" action="/problem" method="post" enctype="multipart/form-data">
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">提问</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="inputEmail3">
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="title">
     </div>
   </div>
   <div class="form-group">
     <label for="inputPassword3" class="col-sm-2 control-label">详细</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="inputPassword3">
+    <div class="col-sm-5">
+      <input type="text" class="form-control" name="content">
     </div>
+  </div>
+  <div class="form-group">
+      <label class="col-sm-2 control-label" for="text">上传图片</label>
+      <div class="col-sm-6">
+          <tr> 
+            <td><input class="common-text" name="image" size="50"  type="file">
+            </td>
+          </tr>
+          <br/>
+          <p class="help-block"><span><i class="fa fa-info-circle"></i> 图片大小2M以内</span></p>
+      </div>
   </div>
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
