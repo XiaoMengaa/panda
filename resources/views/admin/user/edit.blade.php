8f8d78105fb001703e163baa8ffe59eef8b21f19
jiaxu <892092398@qq.com>
2018-09-05 16:22:50 +0800

    用户详情

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -9,7 +9,7 @@
                         <div class='page-header'>
                             <h1 class='pull-left'>
                                 <i class='icon-ok'></i>
-                                <span>用户修改</span>
+                                <span>用户编辑</span>
                             </h1>
                             <div class='pull-right'>
                                 <ul class='breadcrumb'>
@@ -26,7 +26,7 @@
                                     <li class='separator'>
                                         <i class='icon-angle-right'></i>
                                     </li>
-                                    <li class='active'>用户修改</li>
+                                    <li class='active'>用户编辑</li>
                                 </ul>
                             </div>
                         </div>
@@ -39,7 +39,46 @@
                                 <div class='control-group'>
                                     <label class='control-label' for='validation_name'>姓名</label>
                                     <div class='controls'>
-                                        <input data-rule-minlength='2' data-rule-required='true' id='validation_name' name='username' placeholder='Name' type='text' value="{{$user['username']}}" />
+                                        <input data-rule-minlength='2' id='validation_name' name='username' placeholder='Name' type='text' value="{{$user['username']}}" />
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' for='validation_name'>性别</label>
+                                    <div class='controls'>
+                                       <input name="sex" type="radio" @if($udetails) @if($udetails->sex == "m") checked @endif @endif value="m" />男       
+                                        <input name="sex" type="radio"  @if($udetails) @if($udetails->sex == "w") checked @endif @endif value="w" />女
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' data-rule-required='true' for='validation_name'>头像</label>
+                                    <div class='controls'>
+                                        <input type="file" name="pic" id="file"  /><img src="@if($udetails) 
+                                        {{$udetails->pic}}@endif">
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' for='validation_name'>简介</label>
+                                    <div class='controls'>
+                                        <textarea id="txtCon" rows="6" cols="9"  name="synopsis"value="">@if($udetails){{$udetails->synopsis}}@endif</textarea>
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' for='validation_name'>手机号</label>
+                                    <div class='controls'>
+                                        <input data-rule-required='true'   id='' name='phone' placeholder='Name' type='text' maxlength="11" value="@if($udetails){{$udetails->phone}}@endif" />
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' for='validation_name'>邮箱</label>
+                                    <div class='controls'>
+                                        <input data-rule-minlength='2' data-rule-required='true' id='validation_name' name='email' placeholder='Name' type='text' value="@if($udetails){{$udetails->email}}@endif"/>
+                                    </div>
+                                </div>
+                                <div class='control-group'>
+                                    <label class='control-label' for='validation_name'>权限</label>
+                                    <div class='controls'>
+                                        <input name="jurisdiction" type="radio" @if($udetails) @if($udetails->jurisdiction == "1") checked @endif @endif value="1" />管理员      
+                                        <input name="jurisdiction" type="radio"@if($udetails) @if($udetails->jurisdiction == "2") checked @endif @endif value="2" />普通用户
                                     </div>
                                 </div>
                                 
@@ -70,4 +109,3 @@
 
 
 @endsection
-
