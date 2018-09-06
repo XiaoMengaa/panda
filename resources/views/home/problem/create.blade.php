25676ba19b8f60c72893dd0d6679243c61d3ae3b
dingding <dingding@itxdl.cn>
2018-09-06 09:59:26 +0800

    ttt

--- resources/views/home/problem/create.blade.php
+++ resources/views/home/problem/create.blade.php
@@ -15,6 +15,46 @@
             $('#form').ffform({ animation: 'flip', submitButton: '#submit', validationIndicator: '#validation', errorIndicator: '#error', successIndicator: '#success', 'fields': [{ 'id': 'name', required: true, requiredMsg: 'Name is required', type: 'alpha', validate: true, msg: 'Invalid Name' }, { 'id': 'email', required: true, requiredMsg: 'E-Mail is required', type: 'email', validate: true, msg: 'Invalid E-Mail Address' }, { 'id': 'phone', required: false, type: 'custom', validate: false, msg: 'Invalid Phone #' }, { 'id': 'message', required: false, type: 'text', validate: false, msg: ''}] });
         });
     </script>
+    <style>
+          div{
+                //用div的样式代替select的样式
+                width: 200px;
+                height: 40px;
+                border-radius: 5px;
+                //盒子阴影修饰作用,自己随意
+                box-shadow: 0 0 5px #ccc;
+                position: relative;
+            }
+            select{
+                //清除select的边框样式
+                border: none;
+                //清除select聚焦时候的边框颜色
+                outline: none;
+                //将select的宽高等于div的宽高
+                width: 100%;
+                height: 40px;
+                line-height: 40px;
+                //隐藏select的下拉图标
+                appearance: none;
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                //通过padding-left的值让文字居中
+                padding-left: 60px;
+            }
+            //使用伪类给select添加自己想用的图标
+            div:after{
+                content: "";
+                width: 14px;
+                height: 8px;
+                background: url(img/xiala.png) no-repeat center;
+                //通过定位将图标放在合适的位置
+                position: absolute;
+                right: 20px;
+                top: 45%;
+                //给自定义的图标实现点击下来功能
+                pointer-events: none;
+            }
+    </style>
 </head>
 <body style="">
    
@@ -81,7 +121,7 @@
 
             <div class="row  clearfix">
                 <div class="span10 offset2">
-                    <input type="submit" name="submit" id="submit" class="submit" value="提交">
+                    <input type="submit" name="submit" class="submit" value="提交">
                 </div>
             </div>
             </form>
