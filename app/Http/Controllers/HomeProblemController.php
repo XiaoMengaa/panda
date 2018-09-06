d525dce47a1e25bf6fb8583ecf66068a969106dd
xiaoMengaa <1090059082@qq.com>
2018-09-06 20:37:11 +0800

    aa

--- app/Http/Controllers/HomeProblemController.php
+++ app/Http/Controllers/HomeProblemController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
+
 use App\Append;
 use App\Cate;
 use App\Problem;
-=======
-use App\Cate;
-use App\Problem;
-use App\Tag;
->>>>>>> d8573fc4284ddc2c68dffa32897fef3b061934c0
 use App\Reply;
 use App\Tag;
 use Illuminate\Http\Request;
@@ -40,19 +35,12 @@ class HomeProblemController extends Controller
         $cate = Cate::all();
         return view('home.problem.create',['cate'=>$cate]);
     }
-<<<<<<< HEAD
     public function createreply($id)
     {
         $pid = request() -> id;
         
         return view('home.append.create',compact('id','pid')); 
-=======
 
-    public function createreply()
-    {
-
-        return view('home.append.create');
->>>>>>> d8573fc4284ddc2c68dffa32897fef3b061934c0
     }
 
 }
