6c4b131cfd4f5abcf2df62d7bd40dddc1522e7e7
hanguoxi <1054127804@qq.com>
2018-09-10 09:14:03 +0800

    456

--- app/Http/Controllers/HomeProblemController.php
+++ app/Http/Controllers/HomeProblemController.php
@@ -87,13 +87,10 @@ class HomeProblemController extends Controller
             return back()->with('error','登陆失败!');
         }
 
-<<<<<<< HEAD
-=======
         if($Udetails = Udetails::where('user_id','=',$user->id)->get()->first()){
           $Udetails = Udetails::where('user_id','=',$user->id)->get()->first()->jurisdiction;  
         }
         
->>>>>>> b938e816aaae975ca0f2ce8df5a88ff4bd92608b
 
 
         if(Udetails::where('user_id','=',$user->id)->get()->first()){
