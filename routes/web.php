a4303a579d26d8e97b41df24d6bdd8ab73a2eb4a
xiaoMengaa <1090059082@qq.com>
2018-09-17 11:08:40 +0800

    xin

--- routes/web.php
+++ routes/web.php
@@ -158,7 +158,6 @@ Route::get('/home/shangcheng/delete/{id}','HomeShangchengController@delete');
 //前台商城兑换路由
 Route::get('/home/dhjl/create','HomeDhjlController@index');
 
-
 //前台网站配置组
 Route::group(['middleware'=>'spwzsz'],function(){
 
